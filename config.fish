9274bbdcd578117e6a3b5664d9269dd138f7673c
Dylan Scott <dscott@hex.tech>
2023-02-12 16:06:53 -0800

    prompt tweaks

--- config.fish
+++ config.fish
@@ -10,3 +10,23 @@ if status is-interactive
     abbr --add glg git log --stat
     abbr --add grs git reset
 end
+
+# tide prompt
+set --global tide_left_prompt_items pwd newline character
+set --global tide_right_prompt_items cmd_duration status git
+set --global tide_prompt_add_newline_before true
+
+set --global tide_character_icon ❯
+set --global tide_character_vi_icon_default ❮
+set --global tide_character_vi_icon_replace ❮
+set --global tide_character_vi_icon_visual ❮
+
+set --global tide_pwd_color_anchors magenta
+set --global tide_pwd_color_dirs blue
+set --global tide_pwd_color_truncated_dirs brblack
+
+set --global tide_git_truncation_length 256
+set --global tide_git_color_branch magenta
+set --global tide_git_color_upstream blue
+set --global tide_git_color_staged green
+set --global tide_git_color_untracked brblack
